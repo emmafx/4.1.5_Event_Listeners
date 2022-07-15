832e833c70ab9798b1e8526fe5e6b4b6d56f82be
Emma Macleod <97043794+emmafx@users.noreply.github.com>
2022-07-15 14:49:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// greeting() function
+
+function greeting() {
+    console.log("Hello World!"); 
+}
+
+// setTimeout() Example
+
+const myTimeout = setTimeout(greeting, 5000); 
+
+ console.log(myTimeout); 
+
+// setInterval() Example 
+
+const myInterval = setInterval(greeting, 1000); 
+
+console.log(myInterval);
